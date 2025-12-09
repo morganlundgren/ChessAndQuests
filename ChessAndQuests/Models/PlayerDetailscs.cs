60c1ef21171df9c5e7a4aad119e51bf5e976572e
Morgan <morgan01.lundgren@hotmail.se>
2025-12-09 12:40:06 +0100

    added PlayerDetails

---
+++ ChessAndQuests/Models/PlayerDetailscs.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessAndQuests.Models
+{
+    public class PlayerDetailscs
+    {
+        public int PlayerId { get; set; }
+        public string PlayerUserName { get; set; }
+        public string PlayerPassword { get; set; }
+
+    }
+}
