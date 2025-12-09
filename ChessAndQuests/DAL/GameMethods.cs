76a70013ec4164077a348f84b2bf3c5134c382fe
lundg <id23oln@cs.umu.se>
2025-12-09 15:31:31 +0100

    added a constructor that makes the sqlconnection

---
+++ ChessAndQuests/DAL/GameMethods.cs
@@ -0,0 +1,25 @@
+﻿using ChessAndQuests.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ChessAndQuests.DAL
+{
+    
+
+
+    public class GameMethods
+    {
+        private SqlConnection sqlconnection;
+        private string conString;
+        public GameMethods()
+
+        {
+            sqlconnection = new SqlConnection();
+            conString = "Data Source = chesserver.database.windows.net; User ID = adminlogin; Password = ********; Connect Timeout = 30; Encrypt = True; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False";
+            sqlconnection.ConnectionString = conString;
+        }
+        public List<GameDetails> GetAllGames()
+        {
+            String sqlstring ="SELECT * FROM tbl_game";
+        }
+    }
+}
