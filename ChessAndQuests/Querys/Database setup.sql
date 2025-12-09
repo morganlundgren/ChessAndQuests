769def1c295e912eb4c41c31726446309d14f5e8
theo <theo.wikstromm@outlook.com>
2025-12-09 12:29:50 +0100

    added query file

---
+++ ChessAndQuests/Querys/Database setup.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE  [tbl_player]
+(
+	[pl_id] INT NOT NULL PRIMARY KEY,
+	[pl_username] NVARCHAR(100) NOT NULL,
+	[pl_password] NVARCHAR NOT NULL,
+);
+
+CREATE TABLE [tbl_game]
+(
+	[gm_id] INT NOT NULL PRIMARY KEY,
+	[pl_wh_id] INT NOT NULL FOREIGN KEY REFERENCES [tbl_player](pl_id),
+	[pl_bl_id] INT NOT NULL FOREIGN KEY REFERENCES [tbl_player](pl_id),
+	[gm_key] NVARCHAR(100) NOT NULL,
+	[gm_current_fen] NVARCHAR(1000) NOT NULL,
+	[gm_status] INT NOT NULL,
+	[gm_turn] INT NOT NULL
+);
+
