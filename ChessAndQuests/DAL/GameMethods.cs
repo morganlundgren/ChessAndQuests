9d645ead5e42b05b49ce7143a136dc046c0eea50
lundg <id23oln@cs.umu.se>
2025-12-09 16:03:16 +0100

    added a getAllmethod for game

--- ChessAndQuests/DAL/GameMethods.cs
+++ ChessAndQuests/DAL/GameMethods.cs
@@ -1,5 +1,6 @@
 ﻿using ChessAndQuests.Models;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace ChessAndQuests.DAL
 {
@@ -8,18 +9,66 @@ namespace ChessAndQuests.DAL
 
     public class GameMethods
     {
-        private SqlConnection sqlconnection;
+        private SqlConnection sqlConnection;
         private string conString;
         public GameMethods()
 
         {
-            sqlconnection = new SqlConnection();
+            sqlConnection = new SqlConnection();
             conString = "Data Source = chesserver.database.windows.net; User ID = adminlogin; Password = ********; Connect Timeout = 30; Encrypt = True; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False";
-            sqlconnection.ConnectionString = conString;
+            sqlConnection.ConnectionString = conString;
         }
-        public List<GameDetails> GetAllGames()
+        public List<GameDetails> GetAllGames(out string errormsg)
         {
-            String sqlstring ="SELECT * FROM tbl_game";
+            String sqlString ="SELECT * FROM tbl_game";
+            SqlCommand sqlCommand = new SqlCommand(sqlString, sqlConnection);
+
+            SqlDataReader reader = null;
+            List<GameDetails> gameDetailsList = new List<GameDetails>();
+
+            try
+            {
+                // Fyll dataset och mappa rader till modeller
+                sqlConnection.Open();
+                reader = sqlCommand.ExecuteReader();
+               
+                if (!reader.HasRows)
+                {
+                    errormsg = "No data found";
+                    return null;
+                }
+                while (reader.Read())
+                {
+                    GameDetails gameDetails = new GameDetails();
+                    gameDetails.GameId = Convert.ToUInt16(reader["gm_Id"]);
+                    gameDetails.PLayerWhiteId = Convert.ToUInt16(reader["pl_white_id"]);
+                    gameDetails.PlyerBlackId = Convert.ToUInt16(reader["pl_black_id"]);
+                    gameDetails.GameKey = Convert.ToString(reader["gm_key"]);
+                    gameDetails.CurrentFEN = Convert.ToString(reader["gm_current_fen"]);
+                    gameDetails.status = Convert.ToUInt16(reader["gm_status"]);
+                    gameDetails.turnId = Convert.ToUInt16(reader["gm_turn"]);
+
+                    gameDetailsList.Add(gameDetails);
+                }
+                errormsg = "";
+                return gameDetailsList;
+              
+            }
+            catch (Exception e)
+            {
+                // Returnera felmeddelande för att visa vad som gick fel
+                errormsg = e.Message;
+                return null;
+            }
+            finally
+            {
+                if (reader != null && !reader.IsClosed)
+                    reader.Close();
+
+                if (sqlConnection.State == ConnectionState.Open)
+                    sqlConnection.Close();
+            }
         }
+
     }
 }
