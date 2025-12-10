aef63aac274a8a0e947df8d9826d0ef3b0315af2
theo <theo.wikstromm@outlook.com>
2025-12-10 10:08:52 +0100

    test branch

---
+++ ChessAndQuests/DAL/PlayerQuestMethods.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ChessAndQuests.DAL
+{
+    public class PlayerQuestMethods
+    {
+        private SqlConnection sqlConnection;
+        private string conString;
+        public PlayerQuestMethods()
+        {
+            sqlConnection = new SqlConnection();
+            conString = "Data Source = chesserver.database.windows.net; User ID = adminlogin; Password = ********; Connect Timeout = 30; Encrypt = True; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False";
+            sqlConnection.ConnectionString = conString;
+
+        }
+
+        //branch test
+    }
+}
