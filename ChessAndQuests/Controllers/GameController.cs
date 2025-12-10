2087d214d4f10175680fb2813776a53ec1d7e643
theo <theo.wikstromm@outlook.com>
2025-12-10 11:30:25 +0100

    started create game

---
+++ ChessAndQuests/Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChessAndQuests.Controllers
+{
+    public class GameController : Controller
+    {
+        [HttpGet]
+        // create a new game get
+        public IActionResult CreateGame()
+        { 
+
+            return View();
+        }
+    }
+}
