5b0f302c3a2335541b68fe38b84f9f7fbdc09b62
Morgan <morgan01.lundgren@hotmail.se>
2025-12-15 11:32:08 +0100

    made chesslogic

--- ChessAndQuests/wwwroot/js/Game.js
+++ ChessAndQuests/wwwroot/js/Game.js
@@ -14,3 +14,45 @@ connection.on("ReceivePlayerNames", (whiteName, blackName, isWaiting) => {
     document.getElementById("playerBlack").textContent = blackName;
    
 });
+
+var game = new Chess(start_fen);
+var board = null;
+
+function onDragStart(source, piece) {
+        // do not pick up pieces if the game is over
+    if (game.game_over()) return false;
+
+    if ((game.turn() === 'w' && piece.startsWith('b')))) ||
+        (game.turn() === 'b' && piece.startsWith('w'))) {
+        return false;
+
+    }
+}
+
+function onDrop(source, target) {
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q'// NOTE: always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return 'snapback';
+
+    senMoveToServer(source, target, game.fen());
+}
+
+function senMoveToServer(from, to, fen) {
+    fetch('/Game/MakeMove', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            gameKey: gameKey,
+            fromSquare: from,
+            toSquare: to,
+            fen: fen
+        })
+
+
+
+}
