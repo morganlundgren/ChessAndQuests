a91b1dc2f0bbb89fdd030ea6ce60bbacbe3b6485
theo <theo.wikstromm@outlook.com>
2025-12-17 16:03:11 +0100

    css buttons

--- ChessAndQuests/wwwroot/js/Game.js
+++ ChessAndQuests/wwwroot/js/Game.js
@@ -38,7 +38,7 @@ function onDrop(source, target) {
     var move = game.move({
         from: source,
         to: target,
-        promotion: 'q'// NOTE: always promote to a queen for example simplicity
+        promotion: 'q' // always promote to queen 
     });
 
     // illegal move
